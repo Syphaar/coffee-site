68d5e8cdc5b86b19895bcb708de59fc6c032ba61
Sifon Emmanuel <sifonemmanuel123@gmail.com>
2024-06-18 11:37:50 -0700

    first changes

--- main.js
+++ main.js
@@ -15,18 +15,42 @@ function links() {
 };
 
 
+
+
+
 // HERE IS WHERE I'VE BEEN TRYING TO FIX THE ERROR
 // 1). I want the button written as Table booked to alert('table book') and also
 //      clear all the input field once clicked
 // 2). Then the button to show links to different pages is only working on index.html
 
+let form = document.getElementById('form-sec');
+let inputFields = document.getElementsByClassName('input-field');
 
-// const btnEl = document.getElementById('reservation-btn');
 
-let booked = document.getElementById('reservation-btn');
+function sub() {
+    document.getElementById('email-address').value = '';
+    alert('SUBSCRIBED');
+}
 
-function book() {
-    // alert('Table booked');
-};
 
-btnEl.addEventListener('onclick', movedown);
+//
+//
+//
+
+// const cName = document.getElementById('name');
+// const phoneNumber = document.getElementById('phone-number');
+// const time = document.getElementById('time');
+// const email = document.getElementById('eemail');
+// const date = document.getElementById('date');
+// const numberOfPple = document.getElementById('number');
+// const reservationMsg = document.getElementById('reservationMsg');
+// const errorElement = document.getElementById('')
+
+
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     if (cName.value === '' || cName.value == null) {
+//         messages.push('Name is required');
+//     }
+//     e.preventDefault();
+// });
